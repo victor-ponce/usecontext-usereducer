f61b74a06859bc242db6752307c9f5c4998b1e62
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-01-26 15:21:49 -0600

    First Attempt

---
+++ src/components/MyComponent.js
@@ -0,0 +1,41 @@
+//import { useContext } from "react"
+//import { StoreContext } from "../store/StoreProvider";
+import { useDispatch, useStore } from "../store/StoreProvider";
+
+import { types } from "../store/storeReducer";
+
+const MyComponent = () => {
+//const [store, dispatch] = useContext(StoreContext)
+const {user, products} = useStore();
+const dispatch = useDispatch();
+//const { user, products} = store;
+
+  return (
+    <div>
+      <h1>MyComponent</h1>
+      <h2>User: {user?.name}</h2>
+      <button onClick={() => dispatch({ type: types.authLogout})}>
+        Logout
+      </button>
+      <button onClick={() => dispatch({ type: types.authLogin, payload: { id: 1, name: 'Víctor'}})}>
+        Login
+      </button>
+
+      <h2>Products</h2>
+      <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+        
+      </ul>
+      <button onClick={() => dispatch({ type: types.productDeleteAll})}>
+        Delete All
+      </button>
+      <button onClick={() => dispatch({ type: types.productChange})}>
+        Change
+      </button>
+    </div>
+  )
+}
+
+export default MyComponent
